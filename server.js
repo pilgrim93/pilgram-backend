c5c9e6ba43a4bd2959dae841b797a2a9cc5bb4ee
Dré <dre@dres-mbp.mynetworksettings.com>
2025-04-17 14:27:36 -0400

    Add Shoppy webhook route

--- server.js
+++ server.js
@@ -88,3 +88,7 @@ app.post("/shoppy-webhook", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
