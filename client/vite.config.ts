3ecf9670970c0a6aa26104ef53e8907494200770
Dré <dre@dres-mbp.mynetworksettings.com>
2025-04-20 23:54:48 -0400

    “update”

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,9 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import path from 'path';
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   build: {
-    outDir: path.resolve(__dirname, '../dist'),
-    emptyOutDir: true,
-  },
+  optimizeDeps: {
+    exclude: ["@refinedev/simple-rest"]
+  }
 });
